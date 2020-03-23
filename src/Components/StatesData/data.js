8976eb2cadb2356ca149d9f40af47cc242c45c5f
khushal87 <hrithik.agarwal87@gmail.com>
2020-03-23 16:35:26 +0530

    Ui changes

--- src/Components/StatesData/data.js
+++ src/Components/StatesData/data.js
@@ -2,176 +2,184 @@ const data = [
     {
         "SNo": 1,
         "NameOfState": "Andhra Pradesh",
-        "TotalConfirmedCasesIndian": 3,
-        "Total ConfirmedCasesForeign": 0,
+        "TotalConfirmedCasesIndian": 5,
+        "TotalConfirmedCasesForeign": 0,
         "Cured": 0,
         "Death": 0
     },
     {
         "SNo": 2,
+        "NameOfState": "Bihar",
+        "TotalConfirmedCasesIndian": 2,
+        "TotalConfirmedCasesForeign": 0,
+        "Cured": 0,
+        "Death": 1
+    },
+    {
+        "SNo": 3,
         "NameOfState": "Chhattisgarh",
         "TotalConfirmedCasesIndian": 1,
-        "Total ConfirmedCasesForeign": 0,
+        "TotalConfirmedCasesForeign": 0,
         "Cured": 0,
         "Death": 0
     },
     {
-        "SNo": 3,
+        "SNo": 4,
         "NameOfState": "Delhi",
-        "TotalConfirmedCasesIndian": 25,
-        "Total ConfirmedCasesForeign": 1,
+        "TotalConfirmedCasesIndian": 28,
+        "TotalConfirmedCasesForeign": 1,
         "Cured": 5,
         "Death": 1
     },
     {
-        "SNo": 4,
-        "NameOfState": "Gujarat",
-        "TotalConfirmedCasesIndian": 7,
-        "Total ConfirmedCasesForeign": 0,
-        "Cured": 0,
-        "Death": 0
-    },
-    {
         "SNo": 5,
-        "NameOfState": "Haryana",
-        "TotalConfirmedCasesIndian": 3,
-        "Total ConfirmedCasesForeign": 14,
+        "NameOfState": "Gujarat",
+        "TotalConfirmedCasesIndian": 18,
+        "TotalConfirmedCasesForeign": 0,
         "Cured": 0,
-        "Death": 0
+        "Death": 1
     },
     {
         "SNo": 6,
-        "NameOfState": "Himachal Pradesh",
-        "TotalConfirmedCasesIndian": 2,
-        "Total ConfirmedCasesForeign": 0,
+        "NameOfState": "Haryana",
+        "TotalConfirmedCasesIndian": 7,
+        "TotalConfirmedCasesForeign": 14,
         "Cured": 0,
         "Death": 0
     },
     {
         "SNo": 7,
-        "NameOfState": "Karnataka",
-        "TotalConfirmedCasesIndian": 15,
-        "Total ConfirmedCasesForeign": 0,
-        "Cured": 1,
-        "Death": 1
+        "NameOfState": "Himachal Pradesh",
+        "TotalConfirmedCasesIndian": 2,
+        "TotalConfirmedCasesForeign": 0,
+        "Cured": 0,
+        "Death": 0
     },
     {
         "SNo": 8,
+        "NameOfState": "Karnataka",
+        "TotalConfirmedCasesIndian": 26,
+        "TotalConfirmedCasesForeign": 0,
+        "Cured": 2,
+        "Death": 1
+    },
+    {
+        "SNo": 9,
         "NameOfState": "Kerala",
-        "TotalConfirmedCasesIndian": 33,
-        "Total ConfirmedCasesForeign": 7,
+        "TotalConfirmedCasesIndian": 60,
+        "TotalConfirmedCasesForeign": 7,
         "Cured": 3,
         "Death": 0
     },
     {
-        "SNo": 9,
+        "SNo": 10,
         "NameOfState": "Madhya Pradesh",
-        "TotalConfirmedCasesIndian": 4,
-        "Total ConfirmedCasesForeign": 0,
+        "TotalConfirmedCasesIndian": 6,
+        "TotalConfirmedCasesForeign": 0,
         "Cured": 0,
         "Death": 0
-    },
-    {
-        "SNo": 10,
-        "NameOfState": "Maharashtra",
-        "TotalConfirmedCasesIndian": 49,
-        "Total ConfirmedCasesForeign": 3,
-        "Cured": 0,
-        "Death": 1
     },
     {
         "SNo": 11,
-        "NameOfState": "Odisha",
-        "TotalConfirmedCasesIndian": 2,
-        "Total ConfirmedCasesForeign": 0,
+        "NameOfState": "Maharashtra",
+        "TotalConfirmedCasesIndian": 64,
+        "TotalConfirmedCasesForeign": 3,
         "Cured": 0,
-        "Death": 0
+        "Death": 2
     },
     {
         "SNo": 12,
-        "NameOfState": "Puducherry",
-        "TotalConfirmedCasesIndian": 1,
-        "Total ConfirmedCasesForeign": 0,
+        "NameOfState": "Odisha",
+        "TotalConfirmedCasesIndian": 2,
+        "TotalConfirmedCasesForeign": 0,
         "Cured": 0,
         "Death": 0
     },
     {
         "SNo": 13,
+        "NameOfState": "Puducherry",
+        "TotalConfirmedCasesIndian": 1,
+        "TotalConfirmedCasesForeign": 0,
+        "Cured": 0,
+        "Death": 0
+    },
+    {
+        "SNo": 14,
         "NameOfState": "Punjab",
-        "TotalConfirmedCasesIndian": 2,
-        "Total ConfirmedCasesForeign": 0,
+        "TotalConfirmedCasesIndian": 21,
+        "TotalConfirmedCasesForeign": 0,
         "Cured": 0,
         "Death": 1
     },
     {
-        "SNo": 14,
+        "SNo": 15,
         "NameOfState": "Rajasthan",
-        "TotalConfirmedCasesIndian": 15,
-        "Total ConfirmedCasesForeign": 2,
+        "TotalConfirmedCasesIndian": 25,
+        "TotalConfirmedCasesForeign": 2,
         "Cured": 3,
         "Death": 0
     },
     {
-        "SNo": 15,
-        "NameOfState": "Tamil Nadu",
-        "TotalConfirmedCasesIndian": 3,
-        "Total ConfirmedCasesForeign": 0,
-        "Cured": 1,
-        "Death": 0
-    },
-    {
         "SNo": 16,
-        "NameOfState": "Telengana",
-        "TotalConfirmedCasesIndian": 8,
-        "Total ConfirmedCasesForeign": 11,
+        "NameOfState": "Tamil Nadu",
+        "TotalConfirmedCasesIndian": 7,
+        "TotalConfirmedCasesForeign": 2,
         "Cured": 1,
         "Death": 0
     },
     {
         "SNo": 17,
-        "NameOfState": "Chandigarh",
-        "TotalConfirmedCasesIndian": 1,
-        "Total ConfirmedCasesForeign": 0,
-        "Cured": 0,
+        "NameOfState": "Telengana",
+        "TotalConfirmedCasesIndian": 15,
+        "TotalConfirmedCasesForeign": 11,
+        "Cured": 1,
         "Death": 0
     },
     {
         "SNo": 18,
-        "NameOfState": "Jammu and Kashmir",
-        "TotalConfirmedCasesIndian": 4,
-        "Total ConfirmedCasesForeign": 0,
+        "NameOfState": "Chandigarh",
+        "TotalConfirmedCasesIndian": 5,
+        "TotalConfirmedCasesForeign": 0,
         "Cured": 0,
         "Death": 0
     },
     {
         "SNo": 19,
-        "NameOfState": "Ladakh",
-        "TotalConfirmedCasesIndian": 13,
-        "Total ConfirmedCasesForeign": 0,
+        "NameOfState": "Jammu and Kashmir",
+        "TotalConfirmedCasesIndian": 4,
+        "TotalConfirmedCasesForeign": 0,
         "Cured": 0,
         "Death": 0
     },
     {
         "SNo": 20,
-        "NameOfState": "Uttar Pradesh",
-        "TotalConfirmedCasesIndian": 23,
-        "Total ConfirmedCasesForeign": 1,
-        "Cured": 9,
-        "Death": 0
-    },
-    {
-        "SNo": 21,
-        "NameOfState": "Uttarakhand",
-        "TotalConfirmedCasesIndian": 3,
-        "Total ConfirmedCasesForeign": 0,
+        "NameOfState": "Ladakh",
+        "TotalConfirmedCasesIndian": 13,
+        "TotalConfirmedCasesForeign": 0,
         "Cured": 0,
         "Death": 0
     },
     {
+        "SNo": 21,
+        "NameOfState": "Uttar Pradesh",
+        "TotalConfirmedCasesIndian": 27,
+        "TotalConfirmedCasesForeign": 1,
+        "Cured": 9,
+        "Death": 0
+    },
+    {
         "SNo": 22,
+        "NameOfState": "Uttarakhand",
+        "TotalConfirmedCasesIndian": 3,
+        "TotalConfirmedCasesForeign": 0,
+        "Cured": 0,
+        "Death": 0
+    },
+    {
+        "SNo": 23,
         "NameOfState": "West Bengal",
-        "TotalConfirmedCasesIndian": 2,
-        "Total ConfirmedCasesForeign": 0,
+        "TotalConfirmedCasesIndian": 7,
+        "TotalConfirmedCasesForeign": 0,
         "Cured": 0,
         "Death": 0
     }
